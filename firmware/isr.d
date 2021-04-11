102c5501ded7490e9119fa8795e1aaf99b968741
faarodriguezpi <faarodriguezpi@unal.edu.co>
2021-04-10 23:23:30 -0500

    Firmware agregado

---
+++ firmware/isr.d
@@ -0,0 +1,25 @@
+isr.o: isr.c \
+ /home/alex/Digital2/fpga_101/lab004/build/software/include/generated/csr.h \
+ /home/alex/Digital2/fpga_101/lab004/build/software/include/generated/soc.h \
+ /opt/Litex/litex/litex/soc/software/include/base/stdint.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/vexriscv/system.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h \
+ /opt/Litex/litex/litex/soc/software/include/hw/common.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/vexriscv/irq.h \
+ /opt/Litex/litex/litex/soc/software/include/base/uart.h
+
+/home/alex/Digital2/fpga_101/lab004/build/software/include/generated/csr.h:
+
+/home/alex/Digital2/fpga_101/lab004/build/software/include/generated/soc.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/stdint.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/vexriscv/system.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h:
+
+/opt/Litex/litex/litex/soc/software/include/hw/common.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/vexriscv/irq.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/uart.h:
---
+++ firmware/main.d
@@ -0,0 +1,39 @@
+main.o: main.c /opt/Litex/litex/litex/soc/software/include/base/stdio.h \
+ /opt/Litex/litex/litex/soc/software/include/base/stddef.h \
+ /opt/Litex/litex/litex/soc/software/include/base/stdlib.h \
+ /opt/Litex/litex/litex/soc/software/include/base/string.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/vexriscv/irq.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/vexriscv/system.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h \
+ /home/alex/Digital2/fpga_101/lab004/build/software/include/generated/soc.h \
+ /home/alex/Digital2/fpga_101/lab004/build/software/include/generated/csr.h \
+ /opt/Litex/litex/litex/soc/software/include/base/stdint.h \
+ /opt/Litex/litex/litex/soc/software/include/hw/common.h \
+ /opt/Litex/litex/litex/soc/software/include/base/uart.h \
+ /opt/Litex/litex/litex/soc/software/include/base/console.h
+
+/opt/Litex/litex/litex/soc/software/include/base/stdio.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/stddef.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/stdlib.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/string.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/vexriscv/irq.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/vexriscv/system.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h:
+
+/home/alex/Digital2/fpga_101/lab004/build/software/include/generated/soc.h:
+
+/home/alex/Digital2/fpga_101/lab004/build/software/include/generated/csr.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/stdint.h:
+
+/opt/Litex/litex/litex/soc/software/include/hw/common.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/uart.h:
+
+/opt/Litex/litex/litex/soc/software/include/base/console.h:
